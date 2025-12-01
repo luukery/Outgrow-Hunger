2c6f3d3527218982081b12edee956633ca8fdbae
Ouden,Silvan S. den <i515074@fontysict.nl>
2025-12-01 14:25:17 +0100

    add: initial route logic

--- Assets/Scripts/Gameplay/RouteHandler.cs
+++ Assets/Scripts/Gameplay/RouteHandler.cs
@@ -1,88 +1,105 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class RouteHandler : MonoBehaviour
 {
+    [Header("UI References")]
+    public Button shortRouteButton;
+    public Button mediumRouteButton;
+    public Button longRouteButton;
+    public TMP_Text resultText;
+
     List<string> eventsList = new List<string>
     {
-        "Muddy Road",
-        "Heavy Rainfall",
-        "Stormy Weather",
-        "Snowfall",
-        "Thick Fog",
-
-        "Bandits",
-        "Wolves",
-        "Boars",
-        "Horse Panic",
-        "High Tolls",
-
-        "Fallen Trees",
-        "Cows",
-        "Lost Way",
-        "Poor Road Conditions",
-        "Broken Wheel",
+        "Muddy Road", "Heavy Rainfall", "Stormy Weather", "Snowfall", "Thick Fog",
+        "Bandits", "Wolves", "Boars", "Horse Panic", "High Tolls",
+        "Fallen Trees", "Cows", "Lost Way", "Poor Road Conditions", "Broken Wheel",
     };
 
     public class Route
     {
         public string RouteName { get; set; }
         public float TravelTime { get; set; }
+        public int EventChance { get; set; }
 
-        public Route(string routeName, float travelTime)
+        public Route(string routeName, float travelTime, int eventChance)
         {
             RouteName = routeName;
             TravelTime = travelTime;
+            EventChance = eventChance;
         }
     }
 
+    List<Route> routes;
+
     void Start()
     {
-        print("RouteHandler started.");
+        // Generate the three routes when scene loads
+        routes = GenerateRoutes();
+
+        // Assign button listeners
+        shortRouteButton.onClick.AddListener(() => HandleRouteSelection(routes[0]));
+        mediumRouteButton.onClick.AddListener(() => HandleRouteSelection(routes[1]));
+        longRouteButton.onClick.AddListener(() => HandleRouteSelection(routes[2]));
+
+        // Set button labels
+        shortRouteButton.GetComponentInChildren<TMP_Text>().text =
+            $"{routes[0].RouteName}\nTime: {routes[0].TravelTime}h\nEvent: {routes[0].EventChance}%";
+
+        mediumRouteButton.GetComponentInChildren<TMP_Text>().text =
+            $"{routes[1].RouteName}\nTime: {routes[1].TravelTime}h\nEvent: {routes[1].EventChance}%";
+
+        longRouteButton.GetComponentInChildren<TMP_Text>().text =
+            $"{routes[2].RouteName}\nTime: {routes[2].TravelTime}h\nEvent: {routes[2].EventChance}%";
+
+
+        resultText.text = "Choose a route!";
     }
 
-    void Update()
+    void HandleRouteSelection(Route route)
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (EventHappened(route))
         {
-            print(DisplayRoutes());
+            resultText.text = $"EVENT HAPPENED!\n{GetRandomEvent()}";
+        }
+        else
+        {
+            resultText.text = "No event occurred. Safe travels!";
         }
     }
 
     string GetRandomEvent()
     {
+        float roll = Random.value;
+        string rarity = roll < 0.55f ? "Common" : roll < 0.85f ? "Uncommon" : "Rare";
+
         int index = Random.Range(0, eventsList.Count);
-        string randomEvent = eventsList[index];
-        return randomEvent;
+        return $"{rarity}: {eventsList[index]}";
     }
 
     List<Route> GenerateRoutes()
     {
-        // Generate travel times rounded to the nearest 0.25
         float shortRouteTravelTime = Mathf.Round(Random.Range(2f, 4f) * 4f) / 4f;
+        float mediumRouteTravelTime = Mathf.Round(Random.Range(4f, 6f) * 4f) / 4f;
+        float longRouteTravelTime = Mathf.Round(Random.Range(6f, 8f) * 4f) / 4f;
 
-        float mediumRouteTravelTime = Random.Range(4f, 6f);
-        mediumRouteTravelTime = Mathf.Round(mediumRouteTravelTime * 4f) / 4f;
+        int shortRouteEventChance = Random.Range(80, 91);
+        int mediumRouteEventChance = Random.Range(60, 71);
+        int longRouteEventChance = Random.Range(30, 51);
 
-        float longRouteTravelTime = Random.Range(6, 8) * 4 % 1 / 4;
-        longRouteTravelTime = Mathf.Round(longRouteTravelTime * 4f) / 4f;
-
-        Route shortRoute = new("Short Route", shortRouteTravelTime);
-        Route mediumRoute = new("Medium Route", mediumRouteTravelTime);
-        Route longRoute = new("Long Route", longRouteTravelTime);
+        Route shortRoute = new("Short Route", shortRouteTravelTime, shortRouteEventChance);
+        Route mediumRoute = new("Medium Route", mediumRouteTravelTime, mediumRouteEventChance);
+        Route longRoute = new("Long Route", longRouteTravelTime, longRouteEventChance);
 
         return new List<Route> { shortRoute, mediumRoute, longRoute };
     }
 
-    string DisplayRoutes()
+    bool EventHappened(Route route)
     {
-        List<Route> routes = GenerateRoutes();
-        string routeInfo = "Available Routes:\n";
-        foreach (var route in routes)
-        {
-            routeInfo += $"{route.RouteName} - Travel Time: {route.TravelTime} hours\n";
-        }
-        return routeInfo;
+        int roll = Random.Range(1, 101);
+        return roll <= route.EventChance;
     }
 }
