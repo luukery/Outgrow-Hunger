99721cd31d37b2b53ef741fd5130faff0c4f2011
Mauricio Samson <450322@student.fontys.nl>
2025-12-01 15:09:01 +0100

    added distribution game manager

--- Assets/Scripts/DistributionScripts/Distributionmanager.cs
+++ Assets/Scripts/DistributionScripts/Distributionmanager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Distributionmanager : MonoBehaviour
 {
+    public GameObject currentNPC;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
@@ -11,6 +12,14 @@ public class Distributionmanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentNPC != null)
+        {
+            
+        }
+    }
+
+    public void UpdateNPC(GameObject npc)
+    {
+        currentNPC = npc;
     }
 }
--- Assets/Scripts/DistributionScripts/SpawnerScript.cs
+++ Assets/Scripts/DistributionScripts/SpawnerScript.cs
@@ -5,10 +5,11 @@ public class SpawnerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private bool isSpawned = false;
     public GameObject NPCList;
+    public Distributionmanager distributionmanager;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class SpawnerScript : MonoBehaviour
             GameObject npc = Instantiate(NPCList.transform.GetChild(0).gameObject, new Vector3 (0, -4), Quaternion.identity);
             NPCScript npcScript = npc.GetComponent<NPCScript>();
             npcScript.WalkUp = true;
+            distributionmanager.UpdateNPC(npc);
         }   
     }
 
