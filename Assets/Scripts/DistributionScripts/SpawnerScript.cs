28d71cf228bcbd7648a7fffe7c772cc51afb7f05
luukery <luuk.heesbeen@Hotmail.com>
2025-12-02 15:10:07 +0100

    added npc logic

--- Assets/Scripts/DistributionScripts/SpawnerScript.cs
+++ Assets/Scripts/DistributionScripts/SpawnerScript.cs
@@ -2,34 +2,20 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private bool isSpawned = false;
     public GameObject NPCList;
     public Distributionmanager distributionmanager;
+    public GameObject NPC;
 
     void Start()
     {
-
+        GameObject npc = Instantiate(NPC, new Vector3(0, -4), Quaternion.identity);
+        distributionmanager.UpdateNPC(npc);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isSpawned)
-        {
-            isSpawned = true;
-            // npc needs to be random
-            // int rnadnumer = 0;
-            GameObject npc = Instantiate(NPCList.transform.GetChild(0).gameObject, new Vector3 (0, -4), Quaternion.identity);
-            NPCScript npcScript = npc.GetComponent<NPCScript>();
-            npcScript.WalkUp = true;
-            distributionmanager.UpdateNPC(npc);
-        }   
+        
     }
 
-
-    void Despawn()
-    {
-        isSpawned = false;
-    }
 }
