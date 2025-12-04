5c3db1429b609e8c9667919b1562e42f2b4a1865
Mauricio <samsonmauricio@gmail.com>
2025-12-04 11:58:52 +0100

    inverteded dependancy spawner script, buttons change the dialogue but not any changes other than that

--- Assets/Scripts/DistributionScripts/Distributionmanager.cs
+++ Assets/Scripts/DistributionScripts/Distributionmanager.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class Distributionmanager : MonoBehaviour
 {
-    public GameObject currentNPC;
+    public SpawnerScript spawner;
+    public Canvas canvas;
+
+    private Button feedButton, denyButton;
+    private TextMeshProUGUI dialogue;
+
+    private GameObject currentNPC;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
+        SpawnNPC();
+        
+        feedButton = canvas.transform.Find("FeedButton").GetComponent<Button>();
+        denyButton = canvas.transform.Find("DenyButton").GetComponent<Button>();
+        dialogue = canvas.transform.Find("DialogueText").GetComponent<TextMeshProUGUI>();
 
+        feedButton.onClick.AddListener(HandleAccept);
+        denyButton.onClick.AddListener(Deny);
     }
 
     // Update is called once per frame
@@ -14,12 +31,34 @@ public class Distributionmanager : MonoBehaviour
     {
         if (currentNPC != null)
         {
-            
+
         }
     }
-
-    public void UpdateNPC(GameObject npc)
+    
+    private void SpawnNPC()
     {
-        currentNPC = npc;
+        if (currentNPC != null) 
+        {
+            Destroy(currentNPC);
+            Debug.Log("Despawned NPC");
+        }
+
+
+        currentNPC = spawner.SpawnNPC();
+        Debug.Log("Spawned NPC");
+    }
+
+    private void HandleAccept()
+    {
+        // code om eten te geven
+        dialogue.text = "accepted food";
+        SpawnNPC();
+    }
+
+    public void Deny()
+    {
+        // temp text
+        dialogue.text = "Denied NPC Food";
+        SpawnNPC();
     }
 }
--- Assets/Scripts/DistributionScripts/SpawnerScript.cs
+++ Assets/Scripts/DistributionScripts/SpawnerScript.cs
@@ -3,18 +3,13 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject NPCList;
-    public Distributionmanager distributionmanager;
+    // deze is temp, het moet eventueel gewoon een random npc pakken van het npc lijst
     public GameObject NPC;
 
-    void Start()
+    public GameObject SpawnNPC()
     {
-        GameObject npc = Instantiate(NPC, new Vector3(0, -4), Quaternion.identity);
-        distributionmanager.UpdateNPC(npc);
-    }
-
-    void Update()
-    {
-        
+        GameObject npc = Instantiate(NPC, new Vector3(0, 1), Quaternion.identity);
+        return npc;
     }
 
 }
