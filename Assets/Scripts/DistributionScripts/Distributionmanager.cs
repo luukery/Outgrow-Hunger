b0ad90ef5c88ab59a06302131a09343250510643
Mauricio Samson <450322@student.fontys.nl>
2025-12-08 11:06:52 +0100

    Added dropdowns for accepting food, working on linking dropdowns to DTO

--- Assets/Scripts/DistributionScripts/Distributionmanager.cs
+++ Assets/Scripts/DistributionScripts/Distributionmanager.cs
@@ -11,6 +11,8 @@ public class Distributionmanager : MonoBehaviour
 
     private Button feedButton, denyButton;
     private TextMeshProUGUI dialogue;
+    private GameObject foodselect;
+    private List<Dropdown> dropdowns = new();
 
     private NPC currentNPC;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,9 +23,19 @@ public class Distributionmanager : MonoBehaviour
         feedButton = canvas.transform.Find("FeedButton").GetComponent<Button>();
         denyButton = canvas.transform.Find("DenyButton").GetComponent<Button>();
         dialogue = canvas.transform.Find("DialogueText").GetComponent<TextMeshProUGUI>();
+        foodselect = canvas.transform.Find("FoodSelect").gameObject;
+
+        for (int i = 0; i <= 4 ; i++)
+        {
+            string dropdownname = "Dropdown" + i;
+            Dropdown dropdown = foodselect.transform.Find(dropdownname).GetComponent<Dropdown>();
+            dropdowns.Add(dropdown);
+            Debug.Log(dropdown);
+        }
 
         feedButton.onClick.AddListener(HandleAccept);
         denyButton.onClick.AddListener(Deny);
+        foodselect.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,7 +47,7 @@ public class Distributionmanager : MonoBehaviour
         }
     }
 
-    private IEnumerator SpawnNPC()
+    private void SpawnNPC()
     {
         // despawns NPC and waits 5 seconds before spawning a new one if an NPC is already spawned
         if (currentNPC != null)
@@ -43,37 +55,54 @@ public class Distributionmanager : MonoBehaviour
             Destroy(currentNPC.gameObject);
             currentNPC = null;
             Debug.Log("Despawned NPC");
-            yield return new WaitForSeconds(4);
         }
         // Temp text
         currentNPC = spawner.SpawnNPC();
-        HandleDialgoue();
         Debug.Log("Spawned NPC");
     }
 
     private void HandleAccept()
     {
         dialogue.text = "Accepted food";
-        StartCoroutine(SpawnNPC());
-
+        FoodSelector();
         // Check if food can be delivered
     }
+
+    private void FoodSelector()
+    {
+        // steps
+        // take food from npc
+        // change toggle text into food needed
+        // gray out food that dont have enough of 
+        // button change text
+        // button remove listeners
+        // button add new listeners
+
+        foodselect.SetActive(true);
+        // dialogue needs to be reenabled eventually
+        dialogue.gameObject.SetActive(false);
+
+        NpcInfoDTO orderinfo = currentNPC.GetInfoDTO();
+        // cant do a foreach loop bc there's multiple lists in dto 
+        for (int index = 0; index < orderinfo.Needs.Count; index++)
+        {
+            // temp break bc there's only 4 dropdowns
+            if (index >= 5)
+                break;
+            Dropdown dropdown = dropdowns[index];
+            Debug.Log("Searching on: " + dropdown.name);
+            TextMeshProUGUI ordertext = dropdown.transform.Find("OrderText").GetComponent<TextMeshProUGUI>();
+            Request need = orderinfo.Needs[index];
+            Request order = orderinfo.Order[index];
+            ordertext.text = "Need: " + need.Amount + "\nOrder: " + order.Amount;
+        }
+    }
+
 
     public void Deny()
     {
         // temp text
         dialogue.text = "Denied NPC Food";
         StartCoroutine(SpawnNPC());
-    }
-
-    private void HandleDialogue()
-    {
-        // get dto npc request and order
-        currentNPC.getInfoDTO();
-
-        // dialogue displays request and order
-
-
-        // Check if order can be completed, if not disable button/change button text
     }
 }
